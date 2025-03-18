486186ac79e98924f2fd8fc6ed0425e4bb4e9697
Sheharyar. <sheharyar@ucalgary.ca>
2025-03-18 17:24:36 -0600

    dashboard layout

---
+++ apps/frontend/app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,25 @@
+
+import SearchDashboard from "@/components/SearchDashboard"
+
+
+export const metadata = {
+  title: 'Search Dashboard for music',
+  description: 'Main user dashboar for recommendations',
+}
+
+
+export default function Dashboard() {
+
+  return (
+    <>
+      <section>
+
+          <SearchDashboard />
+
+
+     
+      </section>
+
+    </>
+  )
+}
