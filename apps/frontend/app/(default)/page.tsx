23536e3872d10eb48a3fd80ce2a0f7dffcd22a08
Sheharyar. <sheharyar@ucalgary.ca>
2025-03-18 17:03:29 -0600

    default layout fixed and added metadata tags

---
+++ apps/frontend/app/(default)/page.tsx
@@ -0,0 +1,24 @@
+
+
+export const metadata = {
+  title: 'Musify Home',
+  description: 'main landing page for musify',
+}
+
+import Hero from '@/components/hero'
+
+
+
+
+export default function Home() {
+
+
+
+
+  return (
+    <>
+      <Hero />
+
+    </>
+  )
+}
