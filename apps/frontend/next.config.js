d094e4b22dc01779e2561f35723f2b08617b0a92
Sheharyar. <sheharyar@ucalgary.ca>
2025-03-19 06:04:44 -0600

    spotify CDN added to next config

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -4,6 +4,16 @@ const withMDX = require("@next/mdx")();
 const nextConfig = {
   // Configure `pageExtensions` to include MDX files
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  // Add image domains configuration
+  images: {
+    domains: [
+      'i.scdn.co',     // Spotify album art
+      'platform-lookaside.fbsbx.com',
+      'mosaic.scdn.co',
+      'image-cdn-fa.spotifycdn.com',
+      'image-cdn-ak.spotifycdn.com'
+    ],
+  },
   // Optionally, add any other Next.js config below
 };
 
