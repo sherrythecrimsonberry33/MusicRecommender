69024b74c5f1787ac09df0e1529d4a099431d44c
Sheharyar. <sheharyar@ucalgary.ca>
2025-04-08 10:10:07 -0600

    added cors

--- apps/backend/main.py
+++ apps/backend/main.py
@@ -16,7 +16,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://music-recommender-rinuwf459-rbysa.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
